9a0f23911fda2ba025fbc73a0831ae90ae666d39
Rui Machado <ruifilipemachado1997@gmail.com>
2024-11-27 11:57:06 +0000

    Manipulação de erros e descrição dos mesmos em relação à API.

--- api-fetch-app/src/App.js
+++ api-fetch-app/src/App.js
@@ -2,13 +2,14 @@ import useFetch from './useFetch';
 import './App.css';
 
 function App() {
-  const {data, loading} = useFetch("https://jsonplaceholder.typicode.com/users");
+  const {data, loading, error} = useFetch("https://jsonplaceholder.typicode.com/users");
 
   return (
     <div className="App">
       <h1>Fetch API React</h1>
       <div className="card">
         <ul>
+          {error && <li>Error: {error}</li>}
           {loading && <li>Loading...</li>}
           {data?.map((user) => (
             <li key={user.id}>{user.name}</li>
--- api-fetch-app/src/useFetch.js
+++ api-fetch-app/src/useFetch.js
@@ -3,17 +3,18 @@ import { useState, useEffect } from "react";
 export function useFetch(url) {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null); //Estado para detalhar os erros das respostas da API
 
     useEffect(() => {
         setLoading(true);
         fetch(url)
         .then((response) => response.json())
         .then((data) => setData(data))
-        //Executa só quando todas as outras ações/promessas forem executadas ou cumpridas
-        .finally(() => setLoading(false));
+        .catch((error) => setError(error)) //Apanha o erro e faz o que eu definir que deve ser feito nesse caso
+        .finally(() => setLoading(false)); //Executa só quando todas as outras ações/promessas forem executadas ou cumpridas
     }, []);
 
-    return { data, loading };
+    return { data, loading, error };
 }
 
 export default useFetch;
