b6320c0582b15871fe725d2431053d0a7baccb11
Rui Machado <ruifilipemachado1997@gmail.com>
2024-11-27 11:38:29 +0000

    Criação de um loading para ser mostrado quando os dados da API não estão a ser devidamente consumidos.

--- api-fetch-app/src/useFetch.js
+++ api-fetch-app/src/useFetch.js
@@ -2,14 +2,18 @@ import { useState, useEffect } from "react";
 
 export function useFetch(url) {
     const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        setLoading(true);
         fetch(url)
         .then((response) => response.json())
-        .then((data) => setData(data));
+        .then((data) => setData(data))
+        //Executa só quando todas as outras ações/promessas forem executadas ou cumpridas
+        .finally(() => setLoading(false));
     }, []);
 
-    return { data };
+    return { data, loading };
 }
 
 export default useFetch;
